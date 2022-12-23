6eeb4991aa9774ac8c2f1fe6f8981480b577562c
fabio-gabriel <fabiogabrieleig@gmail.com>
2022-12-23 11:31:47 -0300

    fix: fixed navbar functionality

--- src/Components/HomeNavbar.js
+++ src/Components/HomeNavbar.js
@@ -1,21 +1,52 @@
-import { Link } from "react-router-dom";
+import { Link, Route, Routes, useLocation } from "react-router-dom";
 import "../Styles/HomeNavbar.scss";
 
 export default function HomeNavbar() {
+  const location = useLocation();
+
   return (
     <header className="header-home">
       <ul>
         <li>
-          <Link to="/planets"> PLANETS </Link>
+          <Link
+            to="/"
+            className={location.pathname === "/" ? "selected" : null}
+          >
+            HOME
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            to="/planets"
+            className={location.pathname === "/planets" ? "selected" : null}
+          >
+            PLANETS
+          </Link>
         </li>
         <li>
-          <Link to="/characters"> CHARACTERS </Link>
+          <Link
+            to="/characters"
+            className={location.pathname === "/characters" ? "selected" : null}
+          >
+            CHARACTERS
+          </Link>
         </li>
         <li>
-          <Link to="/movies"> MOVIES </Link>
+          <Link
+            to="/movies"
+            className={location.pathname === "/movies" ? "selected" : null}
+          >
+            MOVIES
+          </Link>
         </li>
         <li>
-          <Link to="/species"> SPECIES </Link>
+          <Link
+            to="/species"
+            className={location.pathname === "/species" ? "selected" : null}
+          >
+            SPECIES
+          </Link>
         </li>
       </ul>
     </header>
