a51bc69fffef0fcbd83014e9868eed879d11d788
fabio-gabriel <fabiogabrieleig@gmail.com>
2022-12-23 10:41:52 -0300

    feat: Logo parallax

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,13 @@
+import "../Styles/Home.scss";
+import logo from "../Images/Star_Wars_Logo.png";
+
 function Home() {
   return (
-    <div>
-      <h1> Welcome </h1>
+    <div className="home-page">
+      <div className="hero">
+        <img src={logo} />
+      </div>
+      <h1>Just something</h1>
     </div>
   );
 }
