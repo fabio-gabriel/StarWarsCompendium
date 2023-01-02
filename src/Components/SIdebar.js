0251ae45f5a33a2e7a0b56f64c5514fb094a6442
fabio-gabriel <fabiogabrieleig@gmail.com>
2023-01-02 13:26:28 -0300

    feat: working sidebar request

--- src/Components/SIdebar.js
+++ src/Components/SIdebar.js
@@ -5,13 +5,35 @@ import "../Styles/Sidebar.scss";
 export default function Sidebar() {
   const [list, setList] = useState({ count: 0, results: [] });
 
-  useEffect(() => {
-    api
-      .get("/people")
-      .then((response) => setList(response.data))
-      .catch((err) => {
-        console.error("The API couldn't be reached" + err);
+  async function getResults() {
+    try {
+      let location = "/people";
+      let loop = true;
+      let finalList = [];
+      let finalCount = 0;
+
+      do {
+        const response = await api.get(location);
+
+        if (response.data.results) {
+          finalList = finalList.concat(response.data.results);
+        }
+
+        finalCount = response.data.count;
+        location = response.data.next;
+        loop = response.data.next != null;
+      } while (loop);
+
+      setList({
+        count: finalCount,
+        results: finalList,
       });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  useEffect(() => {
+    getResults();
   }, []);
 
   return (
